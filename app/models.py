f384a878703b8f715a6cf07404560f58456ac19f
William Eichelberger <wceichelb@gmail.com>
2015-09-25 12:13:23 -0700

    display spells on index, pagination

--- app/models.py
+++ app/models.py
@@ -1,8 +1,48 @@
 from app import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True)
     char_name = db.Column(db.String(64), index=True, unique=True)
+    spellbook = db.relationship('Spellbook', backref="user")
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
 
     def __repr__(self):
         return '<User %r>' % (self.char_name)
+
+class Spellbook(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return "<Spellbook %r>" % (self.user.char_name)
+
+class Spell(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    level = db.Column(db.Integer)
+    spell_name = db.Column(db.String)
+    school = db.Column(db.String)
+    casting_time = db.Column(db.String)
+    range = db.Column(db.String)
+    components = db.Column(db.String)
+    duration = db.Column(db.String)
+    # additional_compenents = db.Column(db.String)
+    description = db.Column(db.String)
+    caster_class = db.Column(db.String)
+
+    def __repr__(self):
+        return "<Spell %r>" % (self.spell_name)
--- app/views.py
+++ app/views.py
@@ -1,16 +1,79 @@
-from app import app
-from flask import render_template, g, url_for
+from flask import render_template, flash, redirect, g, url_for, session
+from flask.ext.login import login_user, logout_user, current_user, \
+     login_required
+from datetime import datetime
+from app import app, db, lm, oid
+from .forms import LoginForm
+from .models import User, Spell
+from config import SPELLS_PER_PAGE
+
+@app.before_request
+def before_request():
+    g.user = current_user
+    if g.user.is_authenticated:
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
+
+###############################################################################
+###
+### ROUTES
+###
+###############################################################################
 
 @app.route('/')
 @app.route('/index')
-def index():
-    return render_template('index.html')
+@app.route('/index<int:page>')
+def index(page=1):
+    spells = Spell.query.paginate(page, SPELLS_PER_PAGE, False)
+    return render_template('index.html',
+                           spells=spells
+                           )
 
-@app.route('/spell')
-def spell():
-    return render_template('spell.html')
+# @app.route('/spell')
+# def spell():
+#     return render_template('spell.html')
 
-@app.route('/spellbook')
-@app.route('/spellbook/<user>')
-def spellbook(user=None):
-    return render_template('spellbook.html', user=user)
+# @app.route('/spellbook')
+# @app.route('/spellbook/<user>')
+# def spellbook(user=None):
+#     if user is None:
+#         return render_template('spellbook.html', spell=session.query(User).all())
+#     return render_template('spellbook.html', user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
+def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['char_name'])
+    return render_template('login.html',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('invalid login, try again')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        char_name = resp.char_name
+        if char_name is None or char_name == "":
+            char_name = resp.email.split("@")[0]
+        user = User(char_name=char_name, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember = remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
